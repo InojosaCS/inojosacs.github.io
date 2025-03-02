b0625dd4f8b6635eca883921016fdf0cdba9d31e
InojosaCS <InojosaCS@users.noreply.github.com>
2025-03-02 20:40:28 +0000

    deploy: 47a043640912972c9dd28e61a5d14dcb0d393894

--- app/components/NavLink.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export function NavLink({ href, children }: { href: string; children: React.ReactNode }) {
-    return (
-        <Link href={href} className="text-slate-400 hover:text-[#0bc] transition-colors duration-300">
-            {children}
-        </Link>
-    )
-}
