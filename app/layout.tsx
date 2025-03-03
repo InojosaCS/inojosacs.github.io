4e064844fd1d0eb3f33c0dccb197b426ed1edf79
Cristian Inojosa <cristianinojosa@gmail.com>
2025-03-03 16:39:35 +0100

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className="scroll-smooth">
+            <head>
+                <link rel="icon" href='assets/images/image.png' />
+            </head>
             <body className={inter.className}>{children}</body>
         </html>
     )
